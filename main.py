2bdf7c8e6cd9fa6e22b196338b1d8924223d3cc9
Corinne <CorinneKogut@gmail.com>
2023-12-13 17:50:10 -0500

    finished

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import csv
 import json
+from decimal import *
 
 
 def csv_to_df(file_path):
@@ -19,14 +20,15 @@ def calculate_balances(file_path, df):
             customer_id = int(row['customerId'])
             account_id = int(row['accountId'])
             transaction_type = row['transactionType']
-            amount = float(row['amount'])
+            amount = Decimal(row['amount'])
 
             target_cell = (customer_id, account_id), ('balance')
+            current_balance = Decimal(df.loc[target_cell])
 
             if transaction_type == 'deposit':
-                df.loc[target_cell] += amount
+                df.loc[target_cell] = current_balance + Decimal(amount)
             if transaction_type == 'withdrawal':
-                df.loc[target_cell] -= amount
+                df.loc[target_cell] = current_balance - Decimal(amount)
 
     return df
 
@@ -42,10 +44,16 @@ def format_df_to_list(df):
         if customer_id not in customers:
             customers[customer_id] = {'id': customer_id, 'accounts': []}
 
-        account_data = {'account_id': account_id, 'balance': balance}
+        account_data = {'accountNumber': account_id, 'balance': float(balance)}
         customers[customer_id]['accounts'].append(account_data)
 
     return list(customers.values())
+
+def list_to_JSON(file_path):
+    json_output = json.dumps(customer_list, indent=2)
+
+    with open(file_path, 'w') as output:
+        output.write(json_output)
 
 
 
@@ -54,25 +62,11 @@ def format_df_to_list(df):
 df = csv_to_df('resources/secondset.csv')
 df = calculate_balances('resources/secondset.csv', df)
 customer_list = format_df_to_list(df)
-
-# Convert the list to JSON
-json_output = json.dumps(customer_list, indent=2)
-
-# Writing to sample.json
-with open('resources/output2.json', 'w') as output:
-    output.write(json_output)
-
-
+list_to_JSON('resources/output2.json')
 
 
 # Convert transactions.csv
 df = csv_to_df('resources/transactions.csv')
 df = calculate_balances('resources/transactions.csv', df)
 customer_list = format_df_to_list(df)
-
-# Convert the list to JSON
-json_output = json.dumps(customer_list, indent=2)
-
-# Writing to sample.json
-with open('resources/output1.json', 'w') as output:
-    output.write(json_output)
+list_to_JSON('resources/output1.json')
